ed4a0484613161b837ad11ef7de7543999cecdc5
taisey <taisey02@gmail.com>
2022-06-20 23:15:18 +0900

    connecton, all, signupの雛形を作成

--- map_project/map_app/urls.py
+++ map_project/map_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	#APIのpathを設定する（path, views.api名, name?）
 	path('connection', views.connection, name='connection'),
+	path('all', views.all, name='all'),
+	path('signup', views.signup, name='signup'),
 ]
--- map_project/map_app/views.py
+++ map_project/map_app/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#API:all
+def all(request):
+	result =  'show all node'
+	return HttpResponse(result)
+
+#API:signup
+def signup(request):
+	result =  'signup'
+	return HttpResponse(result)
+
 #API:connection
 def connection(request):
 	result =  'Accept connection'
