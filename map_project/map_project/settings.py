0d7d5bc92214883456961895fde485d60943ff98
taisey <taisey02@gmail.com>
2022-06-20 22:10:05 +0900

    [add] settings.py

--- map_project/map_project/settings.py
+++ map_project/map_project/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'map_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB',
+        'USER': 'team8',
+        'PASSWORD': 'team8',
+         'HOST': '127.0.0.1',
+         'PORT':  '3306',
+
     }
 }
 
