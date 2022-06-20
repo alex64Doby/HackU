12b8d97fd33f47ebe94c26904834185532f2de25
Akihiro Yamaguchi <alex.ay.8823@gmail.com>
2022-06-21 00:53:49 +0900

    API connectionの追加

--- map_project/map_app/views.py
+++ map_project/map_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import hashlib
+
+from requests import request
+
+from sympy import re
 # Create your views here.
 
 #API:all
@@ -24,6 +29,19 @@ def signup(request):
 	return HttpResponse(result)
 
 #API:connection
+#既に繋がっている人の処理を考える
 def connection(request):
-	result =  'Accept connection'
+	request = {
+		"userId1":"user_id3",
+		"userId2":"user_id4", 
+		"status":"status",
+	}
+	userId1 = request["userId1"]
+	userId2 = request["userId2"]
+	status = request["status"]
+	mkhash = userId1 + userId2
+	hs = str(hashlib.md5(mkhash.encode()).hexdigest())
+	from .models import Connections
+	Connections.objects.create(user_id1=userId1, user_id2=userId2, status=status, connection_id=hs)
+	result = Connections.objects.filter(connection_id=hs)
 	return HttpResponse(result)
