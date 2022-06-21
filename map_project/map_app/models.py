6cdd6db4720beb1d1c9fe8f5698d6dcb206fe4c8
taisey <taisey02@gmail.com>
2022-06-21 11:16:48 +0900

    allAPIの実装

--- map_project/map_app/models.py
+++ map_project/map_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-
 class Connections(models.Model):
     connection_id = models.CharField(primary_key=True, max_length=48)
-    user_id1 = models.CharField(max_length=48)
-    user_id2 = models.CharField(max_length=48)
+    user_id1 = models.ForeignKey('Users', models.DO_NOTHING, to_field='user_id', db_column='user_id1', related_name='user_id1')
+    user_id2 = models.ForeignKey('Users', models.DO_NOTHING, to_field='user_id',  db_column='user_id2', related_name='user_id2')
     status = models.CharField(max_length=48)
 
     class Meta:
--- map_project/map_app/views.py
+++ map_project/map_app/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 # Create your views here.
 
 #API:all
 def all(request):
-	result =  'show all node'
-	return HttpResponse(result)
-
+	result = 'ok'
+	from .models import Connections, Users
+	offline_connection_with_pid = Connections.objects.filter(status='offline')
+	online_connection_with_pid = Connections.objects.filter(status='online')
+	offline_pid = [(data.user_id1.prefecture_id, data.user_id2.prefecture_id) for data in offline_connection_with_pid]
+	online_pid = [(data.user_id1.prefecture_id, data.user_id2.prefecture_id) for data in online_connection_with_pid]
+	offline_connections = [[0] * 47] * 47
+	online_connections = [[0] * 47] * 47
+	for id1, id2 in offline_pid:
+		offline_connections[id1][id2] += 1
+	for id1, id2 in online_pid:
+		online_connections[id1][id2] += 1	
+	response = {"offline_connections": offline_connections, "online_connections": online_connections}
+	json_response = json.dumps(response, ensure_ascii=False, indent=2) 
+	return HttpResponse(json_response)
 #API:signup
 def signup(request):
 	request = {
